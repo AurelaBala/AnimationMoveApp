c02e13489d88d19758733993c730dbb1a3f7a483
AurelaBala <aurelatosuni2022@gmail.com>
2023-03-13 21:50:04 -0400

    adding viewWillAppear function

--- AnimationMoveApp/ViewController.swift
+++ AnimationMoveApp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var myLabel: UILabel!
+    @IBOutlet var myTextField: UITextField!
+    @IBOutlet var myImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        myLabel.center.x -= view.bounds.width
+        myTextField.center.x -= view.bounds.width
+        myImageView.center.x -= view.bounds.width
+    }
 
 }
 
